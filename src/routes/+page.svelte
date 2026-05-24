139ebdafa4c2274cdea0b85f12020a00b238b298
AndreaTerenz <andrea.terenziani.at@gmail.com>
2026-05-24 12:17:11 +0200

    mirror test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,6 @@
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the { foo }</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the { foo }
+</p>
