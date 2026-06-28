d8cb24fde1384274969822566b350a760db654a0
AndreaTerenz <andrea.terenziani.at@gmail.com>
2026-06-29 00:14:07 +0200

    remove broken atproto guestbook thing

--- src/routes/guestbook/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { GuestbookSignElement } from "cutebook";
-</script>
-
-<h1>About this site</h1>
-<p>TODO...</p>
-<guestbook-sign did="did:plc:cyd5xuakvgpb72atqinck6jp"></guestbook-sign>
-<guestbook-display did="did:plc:cyd5xuakvgpb72atqinck6jp" limit="50"></guestbook-display>
-<a href="/">Home</a>
