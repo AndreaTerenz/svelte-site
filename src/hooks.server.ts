b8c9e6008c9ca019f0d0b4983b9f257ec774081c
Andrea Terenziani <andrea.terenziani.at@gmail.com>
2026-07-05 15:56:09 +0200

    should copypaste from SO more carefully

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,8 +2,14 @@ import { env } from '$env/dynamic/private';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    if (env.MAINTENANCE_MODE == '1' && event.route.id != '/maintenance')
+    if (env.MAINTENANCE == '1' && event.route.id != '/maintenance'){
+        console.log("maintenance on - Rerouting to /maintenance")
         return new Response(undefined, { status: 302, headers: { location: '/maintenance' } });
+    }
+    else if (env.MAINTENANCE == '0' && event.route.id == '/maintenance'){
+        console.log("maintenance off - Rerouting to /")
+        return new Response(undefined, { status: 302, headers: { location: '/' } });
+    }
 
     // <other logic>
     
--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-    
+    import { beforeNavigate } from '$app/navigation';
+
+    //TODO: add some periodic checks via fetch calls to navigate elsewhere once the site is back online
+
+    beforeNavigate(async ({ cancel }) => {
+        cancel();
+    });
 </script>
 
 <div>
