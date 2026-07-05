bd0364ae869db4e0046f573a6a679e6409bb7eac
Andrea Terenziani <andrea.terenziani.at@gmail.com>
2026-07-05 15:47:23 +0200

    503 page fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/private';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    if (env.MAINTENANCE_MODE != '1' && event.route.id != '/maintenance')
+    if (env.MAINTENANCE_MODE == '1' && event.route.id != '/maintenance')
         return new Response(undefined, { status: 302, headers: { location: '/maintenance' } });
 
     // <other logic>
